807548d70949d89c7c4c0a7a87ac5e7bf8b5e514
Tess Starr <tess.m.starr@gmail.com>
2026-05-28 11:10:24 -0400

    Added Relative Paths

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from getTranscript import MakeTranscript
 from storage_handler import write_json
+from pathlib import Path
+import os 
 
 
 
 if __name__ == "__main__":
     #// TODO It would be great if these could be commant window inputs
-    file = "101Session2Block1" #Name of participants wav file
-    folderpath = 'R:\khri-mehta-lab\Experiments\Projects\Kappa Project\HHF_KappaYear2\ParticipantResponses' 
+    file = "101Session2Block2" #Name of participants wav file
 
-    def getTranscripts (file, folderpath): 
+    currentDir = os.getcwd()
+    wavpath = os.path.join(currentDir, 'WavFiles')
+    transpath = os.path.join(currentDir, 'transcripts')
+
+    try:
+        os.mkdir(transpath)
+        print(f"Directory '{transpath}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{transpath}' already exists.")
+
+
+
+    def getTranscripts (file, wavpath): 
         # Create transcript for participant audiofile 
         filelist = [f"{file}.wav"]
-        partresponses = MakeTranscript (filelist, folderpath) #make transcript
-        write_json(f"{file}.json", partresponses) #Write transcript to json
-        return partresponses 
+ #make transcript
+        if Path(f"{file}.json").exists(): 
+            print ('transcript already exists')
+        else: 
+             
+            partresponses = MakeTranscript (filelist , wavpath )
+            os.chdir(transpath)
+            write_json(f"{file}.json", partresponses) #Write transcript to json
+            return partresponses 
         
-    partresponses= getTranscripts(file, folderpath) #if not make the transcript 
+    partresponses= getTranscripts(file, wavpath) #if not make the transcript 
